b8bf20cb40c735537b988fe2bba763f0f67498a8
Clement Farabet <clement.farabet@gmail.com>
2011-03-09 23:51:11 -0500

    Added luarocks to repo.
    This is a bit large, but really convenient to have by default.

---
+++ luarocks/src/luarocks/config.lua
@@ -0,0 +1,11 @@
+module("luarocks.config")
+LUAROCKS_PREFIX=[[/usr/local]]
+LUA_INCDIR=[[/usr/local/include]]
+LUA_LIBDIR=[[/usr/local/lib]]
+LUA_BINDIR=[[/usr/local/bin]]
+LUAROCKS_SYSCONFIG=[[/usr/local/etc/luarocks/config.lua]]
+LUAROCKS_ROCKS_TREE=[[/usr/local]]
+LUAROCKS_UNAME_S=[[Darwin]]
+LUAROCKS_UNAME_M=[[i386]]
+LUAROCKS_DOWNLOADER=[[wget]]
+LUAROCKS_MD5CHECKER=[[openssl]]
